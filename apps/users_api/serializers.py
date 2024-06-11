9797f2c3b5fc31b6270860e06bc53734a2b6685f
Iri O <oparina.iri@gmail.com>
2024-06-11 18:13:44 +0500

    Updated Serializers

--- apps/users_api/serializers.py
+++ apps/users_api/serializers.py
@@ -2,28 +2,16 @@ from rest_framework import serializers
 from users_api.models import CustomUser
 
 
-class CustomTokenSerializer(serializers.Serializer):
-    email = serializers.EmailField(
-        write_only=True,
-    )
-    password = serializers.CharField(
-        style={'input_type': 'password'},
-        trim_whitespace=False,
-        write_only=True
-    )
-    token = serializers.CharField(
-        read_only=True,
-    )
-
-
 class CustomUserSerializer(serializers.ModelSerializer):
     new_password = serializers.CharField(
         max_length=128,
         required=False,
+        write_only=True,
     )
     password = serializers.CharField(
         max_length=128,
         required=False,
+        write_only=True,
     )
 
     class Meta:
@@ -38,20 +26,16 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
         extra_kwargs = {
             'email': {'read_only': True},
-            'password': {'write_only': True},
-            'new_password': {'write_only': True},
             'thumbnail': {'read_only': True},
         }
 
     def validate(self, attrs):
         new_password = attrs.get('new_password')
         password = attrs.get('password')
-        if new_password is None or password is None:
+        if bool(new_password) != bool(password):
             raise serializers.ValidationError('Для смены пароля нужно ввести старый и новый пароль')
         if new_password and password:
-            if password != self.instance.password:
-                print(password)
-                print(self.instance.password)
+            if not self.instance.check_password(password):
                 raise serializers.ValidationError("Старый пароль неверный")
             attrs['password'] = attrs.pop('new_password')
         return attrs
@@ -69,11 +53,6 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password',
             'confirm_password',
         ]
-
-        extra_kwargs = {
-            'password': {'write_only': True},
-            'confirm_password': {'write_only': True},
-        }
 
     def validate(self, attrs):
         if attrs['password'] != attrs['confirm_password']:
