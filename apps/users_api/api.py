cd4fcdf78759c7144cd0d6b16f42aa44d4f81f24
Iri O <oparina.iri@gmail.com>
2024-06-11 18:10:32 +0500

    Updated views

--- apps/users_api/api.py
+++ apps/users_api/api.py
@@ -1,80 +1,68 @@
-import logging
-
+from rest_framework.parsers import (
+    MultiPartParser,
+    FormParser,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 
-from users_api.serializers import (
-    RegisterSerializer,
-    CustomUserSerializer,
+from users_api.services import (
+    authenticate_user,
+    create_user,
+    update_user,
+    get_user, delete_user,
 )
-from users_api.services import authenticate_user, create_user, update_user
 
 
 class RegisterView(APIView):
     def post(self, request, *args, **kwargs):
-        serializer = RegisterSerializer(
-            data=request.data,
+        status_code, data = create_user(
+            request=request,
         )
-        if serializer.is_valid():
-            create_user(
-                validated_data=serializer.validated_data,
-            )
-            return Response(
-                serializer.data,
-                status=status.HTTP_201_CREATED,
-            )
         return Response(
-            serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
+            data,
+            status=status_code,
         )
 
 
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
-        response = authenticate_user(
+        status_code, data = authenticate_user(
             request=request,
         )
         return Response(
-            data=response['message'],
-            status=response['code'],
+            data,
+            status=status_code,
         )
 
 
 class CustomUserView(APIView):
     permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
 
     def get(self, request, *args, **kwargs):
-        user = request.user
-        serializer = CustomUserSerializer(user)
+        status_code, data = get_user(
+            request=request,
+        )
         return Response(
-            data=serializer.data,
+            data,
+            status=status_code,
         )
 
     def patch(self, request, *args, **kwargs):
-        user = request.user
-        serializer = CustomUserSerializer(
-            user,
-            data=request.data,
+        status_code, data = update_user(
+            request=request,
         )
-        if serializer.is_valid():
-            response = update_user(
-                user=user,
-                validated_data=serializer.validated_data,
-            )
-            return Response(
-                data=response['message'],
-                status=response['code'],
-            )
         return Response(
-            data=serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST,
+            data,
+            status=status_code,
         )
 
-    def delete(self, request, *args, **kwargs): #TODO
-        user = request.user
-        user.delete()
+    def delete(self, request, *args, **kwargs):
+        status_code, data = delete_user(
+            request=request,
+        )
         return Response(
-            status=status.HTTP_204_NO_CONTENT,
+            data,
+            status=status_code,
         )
