1c7ab3775eb9cd7767e24ffc1a67d6d6a2a128ac
Iri O <oparina.iri@gmail.com>
2024-06-12 17:35:27 +0500

    Added response generation

---
+++ utils/response_patterns.py
@@ -0,0 +1,17 @@
+status_messages = {
+    200: 'Успешный успех',
+    400: 'Невалидные данные',
+    401: 'Ошибка авторизации',
+    404: 'Не найдено',
+    500: 'Ошибка сервера',
+}
+
+
+def generate_response(status_code: int, data: dict | None = None) -> (int, dict):
+    return (
+        status_code,
+        {
+            'message': status_messages.get(status_code),
+            'data': data if data else {}
+        }
+    )
