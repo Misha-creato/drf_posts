ac4517bd7b6c66000cd40f2793ec6801f7ec463b
Iri O <oparina.iri@gmail.com>
2024-06-14 18:13:54 +0500

    Added email settings

--- config/settings.py
+++ config/settings.py
@@ -176,20 +176,28 @@ MEDIA_URL = "media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'my_console': {
-            'level': 'DEBUG',
-            'class': 'utils.log_settings.MyConsoleHandler',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['my_console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    },
-}
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get(
+    'EMAIL_HOST', 'smtp.gmail.com'
+)
+EMAIL_HOST_USER = os.environ.get(
+    'EMAIL_HOST_USER', 'example@gmail.com'
+)
+EMAIL_HOST_PASSWORD = os.environ.get(
+    'EMAIL_HOST_PASSWORD', 'password'
+)
+EMAIL_PORT = os.environ.get(
+    'EMAIL_PORT', 587
+)
+EMAIL_USE_TLS = True
+
+# site
+
+SITE_DOMAIN = os.environ.get(
+    'SITE_DOMAIN', '127.0.0.1:8000'
+)
+SITE_PROTOCOL = os.environ.get(
+    'SITE_PROTOCOL', 'http'
+)
