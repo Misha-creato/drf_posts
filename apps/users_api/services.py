752bd9b39fcdab40f7b0f9af93df09f5e63c5e8a
Iri O <oparina.iri@gmail.com>
2024-06-14 18:13:15 +0500

    Updated data access and added funcs fro email sending

--- apps/users_api/services.py
+++ apps/users_api/services.py
@@ -1,7 +1,14 @@
-from typing import Any
+import uuid
 
 from django.contrib.auth import authenticate
+from django.http.request import QueryDict
+from django.urls import reverse
 
+from config.settings import (
+    SITE_PROTOCOL,
+    SITE_DOMAIN,
+)
+from notifications.services import SendMails
 from users_api.models import (
     CustomUser,
     CustomToken,
@@ -11,7 +18,7 @@ from users_api.serializers import (
     CustomUserSerializer,
 )
 
-from utils.log_settings import (
+from utils.logger import (
     get_logger,
     get_log_user_data,
 )
@@ -21,8 +28,7 @@ from utils.response_patterns import generate_response
 logger = get_logger(__name__)
 
 
-def create_user(request: Any) -> (int, dict):
-    data = request.data
+def create_user(data: QueryDict) -> (int, dict):
     user_data = get_log_user_data(
         user_data=data.dict(),
     )
@@ -59,14 +65,15 @@ def create_user(request: Any) -> (int, dict):
     logger.info(
         msg=f'Пользователь {user_data} успешно создан',
     )
+    send_confirm_email(user=user)
     return generate_response(
         status_code=200,
     )
 
 
-def authenticate_user(request: Any) -> (int, dict):
-    email = request.data.get('email')
-    password = request.data.get('password')
+def authenticate_user(data: QueryDict) -> (int, dict):
+    email = data.get('email')
+    password = data.get('password')
     logger.info(
         msg=f'Вход пользователя {email}',
     )
@@ -115,12 +122,13 @@ def authenticate_user(request: Any) -> (int, dict):
     )
 
 
-def get_user(request: Any) -> (int, dict):
-    user = request.user
+def get_user(user: CustomUser) -> (int, dict):
     logger.info(
         msg=f'Получение данных пользователя {user}',
     )
-    serializer = CustomUserSerializer(user)
+    serializer = CustomUserSerializer(
+        instance=user,
+    )
     data = serializer.data
     logger.info(
         msg=f'Данные пользователя {user} успешно получены: {data}',
@@ -131,9 +139,7 @@ def get_user(request: Any) -> (int, dict):
     )
 
 
-def update_user(request: Any) -> (int, dict):
-    user = request.user
-    data = request.data
+def update_user(user: CustomUser, data: QueryDict) -> (int, dict):
     user_data = get_log_user_data(
         user_data=data.dict(),
     )
@@ -142,7 +148,7 @@ def update_user(request: Any) -> (int, dict):
     )
 
     serializer = CustomUserSerializer(
-        user,
+        instance=user,
         data=data,
     )
     if not serializer.is_valid():
@@ -180,8 +186,7 @@ def update_user(request: Any) -> (int, dict):
     )
 
 
-def delete_user(request: Any) -> (int, dict):
-    user = request.user
+def delete_user(user: CustomUser) -> (int, dict):
     email = user.email
     logger.info(
         msg=f'Удаление пользователя {email}',
@@ -203,3 +208,71 @@ def delete_user(request: Any) -> (int, dict):
     return generate_response(
         status_code=200,
     )
+
+
+def set_url_hash(user: CustomUser) -> int:
+    logger.info(
+        msg=f'Установка хэша для пользователя {user}',
+    )
+    url_hash = str(uuid.uuid4())
+    user.url_hash = url_hash
+
+    try:
+        user.save()
+    except Exception as exc:
+        logger.error(
+            msg=f'Не удалось установить хэш для пользователя {user}',
+            exc_info=True,
+        )
+        return 500
+
+    logger.info(
+        msg=f'Хэш для пользователя {user} установлен',
+    )
+    return 200
+
+
+def get_mail_data(user: CustomUser, email_type: str) -> (int, dict):
+    logger.info(
+        msg=f'Получение данных для формирования текста письма {email_type} пользователю {user}',
+    )
+    status = set_url_hash(
+        user=user,
+    )
+    if status != 200:
+        logger.error(
+            msg=f'Не удалось получить данные для формирования текста письма {email_type} \
+            пользователю {user}',
+        )
+        return status, {}
+
+    path = reverse('confirm_email', args=(user.url_hash,))
+    url = f'{SITE_PROTOCOL}://{SITE_DOMAIN}{path}'
+    mail_data = {
+        'url': url,
+    }
+
+    logger.info(
+        msg=f'Данные для формирования текста письма {email_type} \
+        пользователю {user} получены: {mail_data}',
+    )
+    return 200, mail_data
+
+
+def send_confirm_email(user: CustomUser) -> None:
+    email_type = 'confirm_email'
+    status, mail_data = get_mail_data(
+        user=user,
+        email_type=email_type,
+    )
+
+    if status != 200:
+        return generate_response(
+            status_code=status,
+        )
+
+    send_mails = SendMails(email_type=email_type, mail_data=mail_data, recipient=user)
+    status = send_mails.send_mail_to_user()
+    # return generate_response(
+    #     status_code=status,
+    # )
