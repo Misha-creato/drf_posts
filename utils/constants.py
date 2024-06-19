84153ebff66e295641d498b57976c7342032468d
Iri O <oparina.iri@gmail.com>
2024-06-19 18:25:24 +0500

    Updated constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,4 @@
 EMAIL_TYPES = (
     ('confirm_email', 'Подтверждение адреса электронной почты'),
     ('password_reset', 'Восстановление пароля'),
-    ('note_notification', 'Оповещение о заметке'),
 )
