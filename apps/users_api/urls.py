b02cbd87f46a0d07de1c7d8ccfb65b1b889e6aea
Iri O <oparina.iri@gmail.com>
2024-06-19 18:27:06 +0500

    Added paths for password reset

--- apps/users_api/urls.py
+++ apps/users_api/urls.py
@@ -4,6 +4,8 @@ from users_api.api import (
     AuthView,
     CustomUserView,
     ConfirmEmailView,
+    PasswordResetRequestView,
+    PasswordResetView,
 )
 
 
@@ -19,9 +21,19 @@ urlpatterns = [
         name='auth',
     ),
     path(
-        'confirm/email/<str:url_hash>',
+        'confirm/email/<str:url_hash>/',
         ConfirmEmailView.as_view(),
         name='confirm_email',
+    ),
+    path(
+        'password/reset/request/',
+        PasswordResetRequestView.as_view(),
+        name='password_reset_request',
+    ),
+    path(
+        'password/reset/<str:url_hash>/',
+        PasswordResetView.as_view(),
+        name='password_reset',
     ),
     path(
         '',
