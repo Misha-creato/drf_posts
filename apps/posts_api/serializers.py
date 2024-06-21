ece8390fcc822da421d1229d7c5f7102de486ad7
Iri O <oparina.iri@gmail.com>
2024-06-21 18:00:25 +0500

    Updated post serializer

--- apps/posts_api/serializers.py
+++ apps/posts_api/serializers.py
@@ -4,9 +4,20 @@ from posts_api.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author_pk = serializers.CharField(
+        source='author.pk',
+        read_only=True,
+    )
+    author_nickname = serializers.CharField(
+        source='author.nickname',
+        read_only=True,
+    )
+
     class Meta:
         model = Post
         fields = [
+            'author_pk',
+            'author_nickname',
             'title',
             'description',
             'image',
