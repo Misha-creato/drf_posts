07a3ffd6ac37279efff9b4939c60882af7d793a6
Iri O <oparina.iri@gmail.com>
2024-06-21 18:01:33 +0500

    Added new tests

--- apps/posts_api/tests/test_services.py
+++ apps/posts_api/tests/test_services.py
@@ -10,6 +10,9 @@ from posts_api.services import (
     add,
     get_post,
     detail,
+    update,
+    remove,
+    get_posts_by_pk,
 )
 
 CUR_DIR = os.path.dirname(__file__)
@@ -66,7 +69,7 @@ class ServicesTest(TestCase):
         path = f'{self.path}/get_post'
         fixtures = (
             (200, 'valid'),
-            (400, 'invalid'),
+            (404, 'not_found'),
         )
 
         for code, name in fixtures:
@@ -86,7 +89,7 @@ class ServicesTest(TestCase):
         path = f'{self.path}/get_post'
         fixtures = (
             (200, 'valid'),
-            (400, 'invalid'),
+            (404, 'not_found'),
         )
 
         for code, name in fixtures:
@@ -97,6 +100,81 @@ class ServicesTest(TestCase):
 
             status_code, response_data = detail(
                 slug=slug,
+                user=self.user,
+            )
+
+            self.assertEqual(status_code, code, msg=fixture)
+
+    def test_update(self):
+        path = f'{self.path}/update'
+        fixtures = (
+            (200, 'valid'),
+            (400, 'invalid_structure'),
+            (400, 'required_title'),
+            (403, 'forbidden'),
+            (404, 'not_found'),
+        )
+
+        for code, name in fixtures:
+            fixture = f'{code}_{name}'
+
+            with open(f'{path}/{fixture}_request.json') as file:
+                data = json.load(file)
+
+            image_path = data.pop('image_path', None)
+
+            if image_path is not None:
+                with open(f"{self.files}/{image_path}", 'rb') as image:
+                    data['image'] = SimpleUploadedFile(
+                        name=image.name,
+                        content=image.read(),
+                        content_type='image/jpeg',
+                    )
+
+            status_code, response_data = update(
+                slug=data.pop('slug', None),
+                user=self.user,
+                data=data,
+            )
+
+            self.assertEqual(status_code, code, msg=fixture)
+
+    def test_remove(self):
+        path = f'{self.path}/remove'
+        fixtures = (
+            (200, 'valid'),
+            (404, 'not_found'),
+            (403, 'forbidden'),
+        )
+
+        for code, name in fixtures:
+            fixture = f'{code}_{name}'
+
+            with open(f'{path}/{fixture}_request.json') as file:
+                slug = json.load(file)
+
+            status_code, response_data = remove(
+                slug=slug,
+                user=self.user,
+            )
+
+            self.assertEqual(status_code, code, msg=fixture)
+
+    def test_get_posts_by_pk(self):
+        path = f'{self.path}/get_posts_by_pk'
+        fixtures = (
+            (200, 'valid'),
+            (404, 'not_found'),
+        )
+
+        for code, name in fixtures:
+            fixture = f'{code}_{name}'
+
+            with open(f'{path}/{fixture}_request.json') as file:
+                pk = json.load(file)
+
+            status_code, response_data = get_posts_by_pk(
+                pk=pk,
                 user=self.user,
             )
 
